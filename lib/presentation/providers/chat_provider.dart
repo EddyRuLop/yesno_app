3e392df9027afd9df59464c25e193528bd072f9c
Eduardo Ruiz <eduardoyahel@gmail.com>
2025-04-28 12:32:50 -0600

    Starting sendMessage()

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -7,5 +7,23 @@ class ChatProvider extends ChangeNotifier {
     Message(
         text: "Por haberte ido, te vas a especial directo",
         fromWho: FromWho.her),
+    Message(
+        text: "Por haberte ido, te vas a especial directo",
+        fromWho: FromWho.her),
+    Message(
+        text: "Por haberte ido, te vas a especial directo",
+        fromWho: FromWho.her),
   ];
+
+  Future<void> sendMessage(String text) async {
+    final newMessage = Message(
+      text: text,
+      fromWho: FromWho.me,
+    );
+    //Agregar un nuevo mensaje a la lista
+    messageList.add(newMessage);
+
+    //Notifica a provider que algo cambio
+    notifyListeners();
+  }
 }
